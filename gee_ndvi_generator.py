1bebf4c943349fbf5ce591a52fb527e8d6f81710
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-05-03 23:45:47 +0200

    Update gee_ndvi_generator.py

--- gee_ndvi_generator.py
+++ gee_ndvi_generator.py
@@ -42,4 +42,5 @@ def get_ndvi():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
