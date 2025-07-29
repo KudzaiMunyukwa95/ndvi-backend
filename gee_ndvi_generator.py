f9247fa5c5702854f3390715882c1d50f353b097
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-07-29 10:44:16 +0200

    Update gee_ndvi_generator.py

--- gee_ndvi_generator.py
+++ gee_ndvi_generator.py
@@ -36,7 +36,7 @@ EMERGENCE_WINDOWS = {
     "Cotton": (5, 9),
     "Groundnuts": (6, 10),
     "Barley": (7, 11),
-    "Wheat": (13, 17),
+    "Wheat": (9, 13),
     "Millet": (4, 8),
     "Tobacco": (7, 11)  # For nursery emergence
 }
